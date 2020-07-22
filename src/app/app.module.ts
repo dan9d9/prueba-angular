b79e59ac85087e993068b3ccf241762a94dc190b
Daniel Dick <dan9d9@gmail.com>
2020-07-22 13:10:36 +0200

    create initial screens and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { DuenosComponent } from './duenos/duenos.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BusquedaComponent,
+    DuenosComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
