591aadb1ad3359ee00a36eb35ec24d9b10b89cd4
Daniel Dick <dan9d9@gmail.com>
2020-07-24 18:35:02 +0200

    create favorites service

---
+++ src/app/header/favorites.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, EventEmitter } from '@angular/core';
+
+import { User } from '../shared/user-list/user-item/user.model';
+
+@Injectable({ providedIn: 'root' })
+export class FavoritesService {
+  favoritesChanged = new EventEmitter<User[]>();
+
+  private favorites: User[] = [];
+
+  addFavorite(user: User) {
+    this.favorites.push(user);
+    this.favoritesChanged.emit(this.favorites.slice());
+  }
+
+  isCurrentUserAFavorite(userId) {
+    return this.favorites.map((user: User) => user.id).includes(userId);
+  }
+}
